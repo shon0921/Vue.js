4e060f98470eb4ed6607c75a34c724f4a4e534aa
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 09:10:23 +0900

    Create v-bind.vue

---
+++ ch3/v-bind.vue
@@ -0,0 +1,10 @@
+<template>
+  <img v-bind:src="imageUrl" alt="Vue Logo">
+</template>
+<script>
+export default {
+  data()  {
+    return {imageUrl: "https://vuejs.org/images/logo.png"}
+  }
+}
+</script>
