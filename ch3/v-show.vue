83107935c8a1a03e039f14e686f75fdadb150454
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 09:12:25 +0900

    Create v-show.vue

---
+++ ch3/v-show.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="showText">v-if로 보여요</div>
+  <div v-show="showText">v-show로 보여요</div>
+  <button @click="showText = !showText">토글</button>
+</template>
+<script>
+export default {
+  data() {
+    return {showText: true}
+  }
+}
+</script>
