8297484f83cdd6d1ab2bfda2d7a612492f797130
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 09:16:05 +0900

    Create 1.vue

---
+++ Assignment/1.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ text }}</h1>
+  <button v-on:click="changeText">클릭!</button>
+</template>
+<script>
+export default {
+  data() {
+    return { text: 'Hello Vue 3' }
+  },
+  methods: {
+    changeText() {
+      this.text = 'Vue.js는 재밌어요!';
+    }
+  }
+}
+</script>
+
+<!-- 화면에 "Hello Vue 3“ 출력 ({{ }}) --!>
+<!-- 버튼 클릭 시 메시지 변경 (v-on, methods)  --!>
+<!-- 변경 시 메시지는 자유롭게 작성 --!>
