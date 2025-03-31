9a01f2dc79fc0140f9d6a7fa270018403a6d618a
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 10:51:42 +0900

    Create Props(parent).vue

---
+++ ch4/Props(parent).vue
@@ -0,0 +1,16 @@
+<!-- src/App.vue (부모) -->
+<template>
+  <div>
+    <h1>할 일 목록</h1>
+    <todo-item :todo="tesk" />
+  </div>
+</template>
+<script>
+import TodoItem from "./components/TodoItem.vue";
+export default {
+  components: { TodoItem },
+  data() {
+    return { task: 'Vue 배우기' }
+  }
+}
+</script>
