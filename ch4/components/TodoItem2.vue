0573544c788a65d826cd3ecb7a3251aa85ea8124
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 10:56:01 +0900

    Create TodoItem2.vue

---
+++ ch4/components/TodoItem2.vue
@@ -0,0 +1,14 @@
+<!-- src/components/TodoItem2.vue (자식) -->
+<template>
+  <li>{{ todo }}</li>
+</template>
+<script>
+export default {
+  props: ['todo'] // 부모로부터 'todo' 데이터 받기
+}
+</script>
+<style scoped>
+li {
+  color: #333;
+}
+</style>
