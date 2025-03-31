98d1f0dd5940a527b865c09e8888766afd33c5ed
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 12:12:19 +0900

    Create TodoItem4.vue

---
+++ ch4/components/TodoItem4.vue
@@ -0,0 +1,14 @@
+<!-- src/components/TodoItem4.vue -->
+<template>
+  <li>
+    <span :style="{ textDecoration: todo.done ? 'line-through' : 'none'}">
+      {{ todo.text }}
+    </span>
+    <button @click="$emit('toggle', todo)">완료</button>
+  </li>
+</template>
+<script>
+export default {
+  props: ['todo']
+}
+</script>
