87f0619b2925b15de58d833335d01a1ba6a70c77
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 09:12:07 +0900

    Create v-else-if.vue

---
+++ ch3/v-else-if.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="score >= 90">A등급</div>           <!-- 흔히 보는 if else -->
+  <div v-else-if="score >= 80">B등급</div>
+  <div v-else>다시 시도하세요</div>
+</template>
+<script>
+export default {
+  data() {
+    return {score: 80}
+  }
+}
+</script>
