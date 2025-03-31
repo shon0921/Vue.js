bb1a616a5fb9ed4bc422754648cf20ca63bb20f6
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 10:48:49 +0900

    Create SFC.vue

---
+++ ch4/SFC.vue
@@ -0,0 +1,16 @@
+<!-- src/App.vue -->
+<!-- Single File Component --!>
+<template>
+  <ul>
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo.text" />
+  </ul>
+</template>
+<script>
+import TodoItem from "./components/TodoItem.vue";
+export default {
+  components: { TodoItem },
+  data() {
+    return { todos: [{ id: 1, text: 'Vue 배우기'}] }
+  }
+}
+</script>
