c7b956c6eafce5c4c7b00e00709da12245eadb2f
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 11:00:03 +0900

    Create Emit.vue

---
+++ ch4/Emit.vue
@@ -0,0 +1,21 @@
+<!-- src/App.vue (부모) -->
+<template>
+  <div>
+    <h1>할 일 목록</h1>
+    <todo-item :todo="task" @remove="handleRemove" />
+  </div>
+</template>
+<script>
+import TodoItem from "./components/TodoItem3.vue";
+export default {
+  components: { TodoItem },
+  data() {
+    return { task: 'Vue 배우기' }
+  },
+  methods: {
+    handleRemove(todo) {
+      alert(`${todo} 삭제됨!`);
+    }
+  }
+}
+</script>
