997d455d55de445d0e71c2530b6293259511553a
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 10:59:23 +0900

    Create TodoItem3.vue

---
+++ ch4/components/TodoItem3.vue
@@ -0,0 +1,22 @@
+<!-- src/components/TodoItem.vue (자식) -->
+<template>
+  <li>
+    {{ todo }}
+    <button @click="removeTodo">삭제</button>
+  </li>
+</template>
+<script>
+export default {
+  props: ['todo'],
+  methods: {
+    removeTodo() {
+      this.$emit('remove', this.todo);  // 'remove' 이벤트 발생
+    }
+  }
+}
+</script>
+<style scoped>
+li {
+  color: #333;
+}
+</style>
