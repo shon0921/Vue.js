06ea65f911a370c8c4e1eff4e00a1a78151c9b5c
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 12:11:40 +0900

    Create Props&Emit.vue

---
+++ ch4/Props&Emit.vue
@@ -0,0 +1,29 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>할 일 목록</h1>
+    <todo-item
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @toggle="toggleTodo"/>
+  </div>
+</template>
+<script>
+import TodoItem from "./components/TodoItem.vue";
+export default {
+  components: { TodoItem },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'Vue 비우기', done: false }
+      ]
+    }
+  },
+  methods: {
+    toggleTodo(todo) {
+      todo.done = !todo.done;
+    }
+  }
+}
+</script>
