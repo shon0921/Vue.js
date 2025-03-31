b947b2414a6e5e1a48216c5c9f6301971c031e78
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 09:12:49 +0900

    Create v-for.vue

---
+++ ch3/v-for.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'Vue 배우기' },
+        { id: 2, text: '프로젝트 시작하기'}
+      ]
+    }
+  }
+}
+</script>
+
+<!--흔히 보던 for-->
