5dd1c260629f6728b7e7e1c3506728eb71c1246d
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-03-31 10:36:48 +0900

    Create Local_Registration.vue

---
+++ ch4/Local_Registration.vue
@@ -0,0 +1,16 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>Vue 앱</h1>
+    <my-button /> <!-- 어디서든 사용 가능 -->
+  </div>
+</template>
+<script>
+import MyButton from "./components/MyButton2.vue";
+
+export default {
+  components: {
+    MyButton // 지역 등록
+  }
+}
+</script>
