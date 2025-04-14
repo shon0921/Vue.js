31b098631220402a3291fd4cb0a50da77920e95b
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 10:42:39 +0900

    Create ChildComponent.vue

---
+++ ch5/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<!-- src/components/ChildComponent.vue -->
+<template>
+  <div>
+    <p>테마: {{ theme }}</p>
+  </div>
+</template>
+<script>
+import { inject } from 'vue';
+export default {
+  setup() {
+    const theme = inject('theme');
+    return { theme };
+  }
+}
+</script>
