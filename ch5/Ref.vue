5d916a2a2c10aa51dd79f3bb9090430f353257ba
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 09:27:24 +0900

    Create Ref.vue

---
+++ ch5/Ref.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ count }}</p>
+  <button @click="increment">증가</button>
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    };
+    return {count, increment};
+  }
+}
+</script>
