1046e6d57189b3d3eea49a5f74e02304ffc39103
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 09:32:42 +0900

    Create Reactive.vue

---
+++ ch5/Reactive.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ state.count }}  - {{ state.message }}</p>
+</template>
+<script>
+import { reactive } from 'vue';
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      message: 'Hello'
+    });
+    const increment = () => {
+      state.count++;
+    };
+    return { state, increment };
+  }
+}
+</script>
