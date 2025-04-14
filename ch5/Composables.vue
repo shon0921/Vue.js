2f9241d78ff118c66aee1fb3038683bf934159ff
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 10:28:13 +0900

    Create Composables.vue

---
+++ ch5/Composables.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>카운터: {{ count }}</h1>
+    <button @click="increment">증가</button>
+  </div>
+</template>
+<script>
+import { useCounter } from './composables/useCounter';
+
+export default {
+  setup() {
+    const { count, increment } = useCounter();
+    return { count, increment };
+  }
+}
+</script>
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
