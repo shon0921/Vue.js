2c81cd7d30473b37bde656473746884b58c8524f
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 10:42:06 +0900

    Create Provide&inject.vue

---
+++ ch5/Provide&inject.vue
@@ -0,0 +1,18 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>부모</h1>
+    <child-component />
+  </div>
+</template>
+<script>
+import { provide, ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+export default {
+  components: { ChildComponent },
+  setup() {
+    const theme = ref('light');
+    provide('theme', theme);
+  }
+}
+</script>
