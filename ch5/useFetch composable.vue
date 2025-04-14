675055e4631368913ae979fcdffc8733cd26d599
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-04-14 10:53:57 +0900

    Create useFetch composable.vue

---
+++ ch5/useFetch composable.vue
@@ -0,0 +1,28 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>사용자 목록</h1>
+    <button @click="fetchData" :disabled="loading">데이터 가져오기</button>
+    <div v-if="loading">로딩 중...</div>
+    <div v-if="error">에러 : {{ error }}</div>
+    <ul v-if="data">
+      <li v-for="user in data" :key="user.id">{{user.name}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { useFetch } from './composables/useFetch';
+export default {
+  setup() {
+    const {data, error, loading, fetchData} = useFetch(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    return { data, error, loading, fetchData };
+  }
+}
+</script>
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
