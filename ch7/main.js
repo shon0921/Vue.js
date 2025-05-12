703720ca5af36c54fb4c8151b558155e07f88645
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-12 09:30:48 +0900

    Update main.js

--- ch7/main.js
+++ ch7/main.js
@@ -1,13 +1,21 @@
-// Vue 애플리케이션 인스턴스를 생성하기 위한 함수 가져오기
-import { createApp } from 'vue';
-// 최상위 App 컴포넌트 사져오기
-import App from './App.vue';
-import MyButton from './components/MyButton.vue';
-// 설정한 라우터 인스턴스 가져오기
-import router from './router'
+// Vue Router에서 라우터 생성 및 히스토리 모드 설정을 위한 함수 가져오기
+import { createRouter, createWebHistory } from 'vue-router'
+// 총 페이지에 렌더링할 컴포넌트 가져오기
+import HomeView from '../views/HomeView.vue'
 
-const app = createApp(App).use(router);
-app.component('MyButton', MyButton);  // 글로벌 등록
-app.mount('#app');
+// 라우터 인스턴스 생성
+const router = createRouter({
+  // HTML5 히스토리 모드를 사용하여 깔끔한 URL 제공 (예: /about)
+  history: createWebHistory(process.env.BASE_URL),
+  // 애플리케이션의 경로와 컴포넌트를 매핑하는 라우트 배열
+  routes: [
+    {
+      path: '/', //루트경로
+      name: 'home', // 라우트 이름
+      component: HomeView // 렌더링 컴포넌트
+    }
+  ]
+});
 
-// createApp(App).mount('#app')
+// 라우터 인스턴스를 다른 파일에서 사용할 수 있도록 내보내기
+export default router
