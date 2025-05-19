c9d54f48c66c49fe73eeeedb4a9939aba4d13b03
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 10:50:31 +0900

    유효성 검증 함수

---
+++ ch10/V-model6.vue
@@ -0,0 +1,17 @@
+<template>
+  <input v-model="email" placeholder="이메일 입력">
+  <p v-if="email && !isValidEmail(email)">올바른 이메일 형식을 입력해주세요.</p>
+</template>
+<script>
+export default {
+  data() {
+    return { email: "" }
+  },
+methods: {
+    isValidEmail(email) {
+      
+      return /\s+@\s+\/\s+/.test(email); 
+    }
+  }
+}
+</script>
