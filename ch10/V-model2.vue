c37c68e4a39ec38715379673df1514e4d62e591c
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 09:29:36 +0900

    체크박스

---
+++ ch10/V-model2.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="checkbox" v-model="isChecked">
+  <p>체크 상태: {{ isChecked }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return { isChecked: false }
+  }
+}
+</script>
