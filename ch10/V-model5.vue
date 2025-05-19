79a7ce42952c11d57f70d5b97059425862bc3124
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 10:49:43 +0900

    유효성 검증 if-else

---
+++ ch10/V-model5.vue
@@ -0,0 +1,11 @@
+<template>
+  <input v-model="username" placeholder="아이디 입력">
+  <p v-if="!username">아이디를 입력해주세요.</p>
+</template>
+<script>
+export default {
+  data() {
+    return { username: '' }
+  }
+}
+</script>
