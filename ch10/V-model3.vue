29d20b2769f6c052611460267bc8b526c0523c64
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 09:31:41 +0900

    드롭다운

---
+++ ch10/V-model3.vue
@@ -0,0 +1,14 @@
+<template>
+  <select v-model="selected">
+    <option value="option1">옵션 1</option>
+    <option value="option2">옵션 2</option>
+  </select>
+  <p>선택된 옵션: {{ selected }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return { selected: "option1" }
+  }
+}
+</script>
