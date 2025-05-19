0d8ee295f1252eee501089dc8057081cbd01bbb5
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 09:34:40 +0900

    다중 입력 폼

---
+++ ch10/V-mode4l.vue
@@ -0,0 +1,15 @@
+<template>
+  <input v-model="firstName" placeholder="이름">
+  <input v-model="lastName" placeholder="성">
+  <p>입력된 이름 {{ firstName }} {{ lastName }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+    }
+  }
+}
+</script>
