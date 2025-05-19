23c363b30f3d9e745bc314c58ab23b7d819f1414
shon0921 <43308838+shon0921@users.noreply.github.com>
2025-05-19 09:27:45 +0900

    Create V-model.vue

---
+++ ch10/V-model.vue
@@ -0,0 +1,11 @@
+<template>
+  <input v-model="message" placeholder="Y택스트를 입력하세요">
+  <p>입력된 텍스트: {{ message }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return { message: '' }
+  }
+}
+</script>
